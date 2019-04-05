5404b02e0f9bb5c6a7a04d68f981cea3b5d01861
ValeriiaZub <45237320+ValeriiaZub@users.noreply.github.com>
2019-04-05 12:29:03 -0400

    changes

--- js/notes.js
+++ js/notes.js
@@ -1,22 +1,28 @@
  new Vue({
-            el: '#app',
-            data: {
-                newItem: '',
-                items: [],
-                title: 'My to-do list'
-            },
-            methods: {
-                addItem: function() {
-                    var text;
-                    text = this.newItem.trim();
-                    if (text) {
-                        this.items.push({
-                            text: text,
-                            checked: false
-                        });
-                        this.newItem = '';
-                    }
-                }
-            }
+     el: '#app',
+     data: {
+         newItem:'',
+         items: [],
+         title: 'My to-do list',
+     },
+     methods: function () {
+         if (isLocalStorage() /* function to detect if localstorage is supported*/ ) {
+             localStorage.setItem('YourItem', this.input)
+            
+         }
+     },
+     methods: {
+         addItem: function () {
+             var text;
+             text = this.newItem.trim();
+             if (text) {
+                 this.items.push({
+                     text: text,
+                     checked: false
+                 });
+                 this.newItem = '';
+             }
+         }
+     }
 
-        });
+ });
