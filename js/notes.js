b74d34357bb6fd40a92b6cf6bc9fab5198733a90
ValeriiaZub <45237320+ValeriiaZub@users.noreply.github.com>
2019-04-15 22:14:23 -0400

    changes

--- js/notes.js
+++ js/notes.js
@@ -1,38 +1,78 @@
+//new Vue({
+//    el: '#app',
+//    data: {
+//        newItem: '',
+//        items: [],
+//        title: 'My to-do list',
+//    },
+//    mounted() {
+//        if (localStorage.getItem('items')) {
+//            try {
+//                this.items = JSON.parse(localStorage.getItem('items'));
+//            } catch (e) {
+//                localStorage.removeItem('items');
+//            }
+//        }
+//    },
+//    methods: {
+//        addItem: function () {
+//            var text;
+//            text = this.newItem.trim();
+//            if (text) {
+//                this.items.push({
+//                    text: text
+//                });
+//                this.newItem = '';
+//                this.saveItems();
+//            }
+//        },
+//        deleteItem(x){
+//            this.items.splice(x, 1);
+//            this.saveItems();
+//        },
+//        saveItems() {
+//            const parsed = JSON.stringify(this.items);
+//            localStorage.setItem('items', parsed);
+//        }
+//    }
+//
+//});
+
 new Vue({
-    el: '#app',
+    el: '#app1',
     data: {
         newItem: '',
-        items: [],
+        items1: [],
         title: 'My to-do list',
     },
     mounted() {
-        if (localStorage.getItem('items')) {
+        if (localStorage.getItem('items1')) {
             try {
-                this.items = JSON.parse(localStorage.getItem('items'));
+                this.items = JSON.parse(localStorage.getItem('items1'));
             } catch (e) {
-                localStorage.removeItem('items');
+                localStorage.removeItem('items1');
             }
         }
     },
     methods: {
-        addItem: function () {
+        addItem1: function () {
             var text;
             text = this.newItem.trim();
             if (text) {
-                this.items.push({
+                this.items1.push({
                     text: text
                 });
                 this.newItem = '';
-                this.saveItems();
+                this.saveItems1();
             }
         },
-        deleteItem(x){
-            this.items.splice(x, 1);
-            this.saveItems();
+        deleteItem1(x){
+            this.items1.splice(x, 1);
+            this.saveItems1();
         },
-        saveItems() {
-            const parsed = JSON.stringify(this.items);
-            localStorage.setItem('items', parsed);
+        saveItems1() {
+            const parsed = JSON.stringify(this.items1);
+            localStorage.setItem('items1', parsed);
         }
     }
 
